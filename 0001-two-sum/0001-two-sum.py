cf7fc9bebe4e883ea784c0939e7ad689c65276c8
Ajay Singh Mehta <69183207+AjaySinghMehta@users.noreply.github.com>
2024-08-09 17:23:02 +0530

    Time: 63 ms (51.25%), Space: 17.7 MB (26.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,14 @@
-# using dicitonaries faster than O(n^2)
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #using dobule for loop brute force
+        # for i in range(0,len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return i,j
+        # return -1
+        
+        # using dicitonaries faster than O(n^2)
         visited = {}
         for i , num in enumerate(nums):
             if target - num in visited:
