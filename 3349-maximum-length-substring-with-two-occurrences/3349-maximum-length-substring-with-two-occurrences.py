0891032d4857a6ea30078a82af44574bbebab854
Ajay Singh Mehta <69183207+AjaySinghMehta@users.noreply.github.com>
2024-12-20 11:46:00 +0530

    Time: 11 ms (26.32%), Space: 17.8 MB (9.41%) - LeetHub

--- 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
@@ -2,24 +2,27 @@ class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         n = len(s)
         l = 0
-        dict = {}
-        maxi = 0
+        r = 0
+        char_freq = {}
+        max_length = 0
 
-        for r in range(n):
-            # Add the current character to the frequency dictionary
-            if s[r] in dict:
-                dict[s[r]] += 1
-            else:
-                dict[s[r]] = 1
-            
-            # Shrink the window if any character occurs more than twice
-            while any(freq > 2 for freq in dict.values()):
-                dict[s[l]] -= 1
-                if dict[s[l]] == 0:
-                    del dict[s[l]]
-                l += 1
-            
-            # Update the maximum length of the valid substring
-            maxi = max(maxi, r - l + 1)
-        
-        return maxi
+        while r < n:
+            char_freq[s[r]] = char_freq.get(s[r], 0) + 1
+
+            while True:
+                found = False
+                for char, freq in char_freq.items():
+                    if freq > 2:
+                        found = True
+                        char_freq[s[l]] -= 1
+                        if char_freq[s[l]] == 0:
+                            del char_freq[s[l]]
+                        l += 1
+                        break
+                if not found:
+                    break
+
+            max_length = max(max_length, r - l + 1)
+            r += 1
+
+        return max_length
