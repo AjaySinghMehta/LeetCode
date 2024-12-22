034297c92a54b06a8b8f819daf89f101363c7e7b
Ajay Singh Mehta <69183207+AjaySinghMehta@users.noreply.github.com>
2024-12-22 16:29:18 +0530

    Time: 36 ms (44.01%), Space: 17.6 MB (5.93%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.py
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -1,24 +1,34 @@
 class Solution:
+    def is_nice(substr):
+        for i in substr:
+            if(i.upper() not in substr or i.lower() not in substr):
+                return False
+        return True
     def longestNiceSubstring(self, s: str) -> str:
         # brute force
         n = len(s)
         maxi = ""
-        temp = []
+        # temp = []
+        # for i in range(n):
+        #     for j in range(i+1,n+1):
+        #         temp.append(s[i:j])
+        # t = []
+        # for i in temp:
+        #     flag = 0
+        #     for j in range(len(i)):
+        #         if chr(ord(i[j]) - 32) in i or chr(ord(i[j]) + 32) in i:
+        #            flag = 1
+        #         else:
+        #             flag = 0
+        #             break
+        #     if(flag and len(maxi)<len(i)):
+        #         maxi = i
+        # print(temp)
         for i in range(n):
-            for j in range(i+1,n+1):
-                temp.append(s[i:j])
-        t = []
-        for i in temp:
-            flag = 0
-            for j in range(len(i)):
-                if chr(ord(i[j]) - 32) in i or chr(ord(i[j]) + 32) in i:
-                   flag = 1
-                else:
-                    flag = 0
-                    break
-            if(flag and len(maxi)<len(i)):
-                maxi = i
-        print(temp)
+            for j in range(i+1, n+1):
+                sub = s[i:j]
+                if(Solution.is_nice(sub) and len(sub)>len(maxi)):
+                    maxi = sub
         return maxi
 
                 
