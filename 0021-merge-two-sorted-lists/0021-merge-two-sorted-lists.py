b5e8e2a177d163b7af2a28e83e4f1c48e755ac5c
Ajay Singh Mehta <69183207+AjaySinghMehta@users.noreply.github.com>
2024-12-23 21:04:14 +0530

    Time: 0 ms (100%), Space: 17.8 MB (5.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,17 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merg(self, list1, list2):  # Added 'self' to the method
+    def merg(self, list1, list2):  
         if not list1:
             return list2
         if not list2:
             return list1
 
         if list1.val <= list2.val:
-            list1.next = self.merg(list1.next, list2)  # Use 'self.merg' to call the instance method
+            list1.next = self.merg(list1.next, list2)  
             return list1
         else:
-            list2.next = self.merg(list1, list2.next)  # Use 'self.merg' to call the instance method
+            list2.next = self.merg(list1, list2.next)  
             return list2
 
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
